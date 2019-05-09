26058dc67f40a3097ab78b91f3952f8f3cbcc8c0
Jaydeep Tank <jaydeep.tank2@techdata.com>
2019-05-09 18:38:41 +0530

    toprange updated

--- src/app/spardha/topRankers/toprankers.component.ts
+++ src/app/spardha/topRankers/toprankers.component.ts
@@ -44,6 +44,7 @@ export class TopRankers implements OnInit{
             slot_marks_submitted: ""
         }
     }
+    Marks: any = [];
     topRangegroup: any = ['10', '20', '30', '40', '50']
     minMaxRange:any = ['10', '20', '30', '40', '50', '60', '70', '80', '90', '100']
     display = 'none'; //default Variable
@@ -56,24 +57,26 @@ export class TopRankers implements OnInit{
 
     }
     
-constructor(
-                private router: Router,
-                private spardhaservice: SpardhaService,
-                private alertService: AlertService,
-                private modalService: NgbModal,
-               
-                ) {
+    constructor(
+        private router: Router,
+        private spardhaservice: SpardhaService,
+        private alertService: AlertService,
+        private modalService: NgbModal,
+        private formBuilder: FormBuilder
+    ) {
         
     }
     @ViewChild('contentToConvert') contentToConvert: ElementRef;
     ngOnInit() {
         this.searchMarkesForm = new FormGroup({
-            'minMarks': new FormControl('', [
-                Validators.required ])
+            minMarks: new FormControl('', [Validators.required, Validators.min(3), Validators.max(100) ] ),
+            maxMarks: new FormControl('', [Validators.required, Validators.min(3), Validators.max(100)])
             
         });
-        
 
+        //this.searchMarkesForm.controls['minMarks'].valueChanges.subscribe(value => {
+        //    console.log(value);
+        //});
        
         //this.spardhaservice.getUniqueSaprdhaName()
         //     .pipe()
@@ -90,6 +93,14 @@ constructor(
         //         });
     }
     get f() { return this.searchMarkesForm.controls; };
+    get minMarks() {
+        return this.searchMarkesForm.get('minMarks') as FormControl;
+    }
+
+    get maxMarks() {
+        return this.searchMarkesForm.get('maxMarks') as FormControl;
+    }
+    
     _getSpardhaDate(spardhasname){
         this.selectedSaprdha=spardhasname;
         //this.spardhaservice.getSpardhaDate(spardhasname)
@@ -109,13 +120,63 @@ constructor(
    
     onSubmit() {
         this.submitted = true;
-
+        let minMarkesValue = this.searchMarkesForm.controls['minMarks'].value;
+        let maxMarkesValue = this.searchMarkesForm.controls['maxMarks'].value;
+        console.log(this.searchMarkesForm.controls['minMarks'].value)
         // stop here if form is invalid
+
+        if (minMarkesValue > maxMarkesValue) {
+            this.maxMarks.setErrors({ 'incorrect': true });
+            
+        }
+        /// backend call
+        this.Marks = [
+            {
+                "mark_id": "2",
+                "mark_bal_id": "2212",
+                "mark_spardha": "1",
+                "mark_marks": "80",
+                "mark_adhivation_type": "Semi",
+                "mark_selected4final": "No",
+                "mark_remark": null,
+                "slot_spardha": "Pravachan",
+                "slot_date": "2019-05-01",
+                "slot_from_time": "12:10:00",
+                "slot_to_time": "14:10:00",
+                "slot_place": "Vidhyamandir School",
+                "slot_room": "1",
+                "bal_surname": "Patel",
+                "bal_name": "Akshar",
+                "bal_father": "Dineshbhai",
+                "bal_phone": "9428518750"
+            },
+            {
+                "mark_id": "1",
+                "mark_bal_id": "2211",
+                "mark_spardha": "1",
+                "mark_marks": "50",
+                "mark_adhivation_type": "Semi",
+                "mark_selected4final": "No",
+                "mark_remark": null,
+                "slot_spardha": "Pravachan",
+                "slot_date": "2019-05-01",
+                "slot_from_time": "12:10:00",
+                "slot_to_time": "14:10:00",
+                "slot_place": "Vidhyamandir School",
+                "slot_room": "1",
+                "bal_surname": "Jethva",
+                "bal_name": "Saral",
+                "bal_father": "Harikrishnabhai",
+                "bal_phone": "9998816300"
+            }
+        ]
+        
+        
         if (this.searchMarkesForm.invalid) {
             return;
         }
 
-        alert('SUCCESS!! :-)')
+        
     }
     
     ngAfterViewInit(){
