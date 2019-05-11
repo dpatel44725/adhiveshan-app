c2f062abdc3e4905e29577b420f0f67c25e340c0
Divyesh <divpatel@kohesiv.com>
2019-05-11 23:16:58 +0530

    resolved issue

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -17,8 +17,6 @@ import { LoginComponent } from './login/login.component';
         FormsModule
     ],
     declarations: [
-        LoginComponent,
-        RegisterComponent,
         LockComponent
     ]
 })
